c12b65152141252415c98ec613261c8e5351779d
sabitov.a <sabitov.a@otbasybank.kz>
2024-08-12 09:48:45 +0500

    [fix] swagger

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 // @ts-ignore
 import puppeteer from 'puppeteer';
@@ -10,12 +10,12 @@ const app = express();
 const port = process.env.PORT || 1984;
 const executablePath = process.env.PUPPETEER_EXECUTABLE_PATH || '';
 
-app.use(cors({ origin: '*' }));
+app.use(cors({origin: '*'}));
 
 const swaggerOptions = {
     definition: {
         openapi: '3.0.0',
-        info: { title: 'PDF Generator', version: '1.0.0' },
+        info: {title: 'Linux PDF Generator', version: '1.0.0'},
     },
     apis: ['./src/**/*.ts'],
 };
@@ -23,8 +23,8 @@ const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
 
 const launchBrowser = async () => {
     return puppeteer.launch({
@@ -56,12 +56,14 @@ const generatePdf = async (html: string, margin: any, headerTemplate?: string, f
     await browser.close();
     return pdfBuffer.toString('base64');
 };
-
 /**
  * @swagger
  * /api/v1/generate-pdf:
  *   post:
- *     summary: Generate PDF from HTML
+ *     tags:
+ *       - Generation
+ *     summary: Генерация PDF из HTML
+ *     description: Этот метод позволяет сгенерировать PDF-документ на основе переданного HTML-кода.
  *     requestBody:
  *       content:
  *         application/json:
@@ -70,28 +72,42 @@ const generatePdf = async (html: string, margin: any, headerTemplate?: string, f
  *             properties:
  *               html:
  *                 type: string
+ *                 description: HTML-код основного содержимого документа
  *               margin:
  *                 type: object
+ *                 description: Отступы страницы в миллиметрах
  *                 properties:
  *                   top:
  *                     type: number
+ *                     description: Отступ сверху
  *                   right:
  *                     type: number
+ *                     description: Отступ справа
  *                   bottom:
  *                     type: number
+ *                     description: Отступ снизу
  *                   left:
  *                     type: number
+ *                     description: Отступ слева
  *     responses:
  *       200:
- *         description: Base64-encoded PDF
+ *         description: PDF-документ в Base64
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 pdfBase64:
+ *                   type: string
+ *                   description: Base64-кодированный PDF-документ
  */
 app.post('/api/v1/generate-pdf', async (req: Request, res: Response) => {
-    const { html, margin } = req.body;
+    const {html, margin} = req.body;
     try {
         const pdfBase64 = await generatePdf(html, margin);
-        res.json({ pdfBase64 });
+        res.json({pdfBase64});
     } catch (error) {
-        res.status(500).json({ error: 'Failed to generate PDF' });
+        res.status(500).json({error: 'Не удалось сгенерировать PDF'});
     }
 });
 
@@ -99,7 +115,10 @@ app.post('/api/v1/generate-pdf', async (req: Request, res: Response) => {
  * @swagger
  * /api/v1/generate-pdf-with-header-footer:
  *   post:
- *     summary: Generate PDF from HTML with header and footer
+ *     tags:
+ *       - Generation
+ *     summary: Генерация PDF из HTML с хедером и футером
+ *     description: Этот метод позволяет сгенерировать PDF-документ на основе HTML-кода. Хедеры и футеры должны быть переданы в виде HTML-кода.
  *     requestBody:
  *       content:
  *         application/json:
@@ -108,26 +127,55 @@ app.post('/api/v1/generate-pdf', async (req: Request, res: Response) => {
  *             properties:
  *               html:
  *                 type: string
+ *                 description: HTML-код основного содержимого документа
  *               margin:
  *                 type: object
+ *                 description: Отступы страницы в миллиметрах
+ *                 properties:
+ *                   top:
+ *                     type: number
+ *                     description: Отступ сверху
+ *                   right:
+ *                     type: number
+ *                     description: Отступ справа
+ *                   bottom:
+ *                     type: number
+ *                     description: Отступ снизу
+ *                   left:
+ *                     type: number
+ *                     description: Отступ слева
  *               headerTemplate:
  *                 type: string
+ *                 description: HTML-код для хедера (верхнего колонтитула)
  *               footerTemplate:
  *                 type: string
+ *                 description: HTML-код для футера (нижнего колонтитула)
+ *             required:
+ *               - html
+ *               - margin
  *     responses:
  *       200:
- *         description: Base64-encoded PDF
+ *         description: PDF-документ в Base64
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 pdfBase64:
+ *                   type: string
+ *                   description: Base64-кодированный PDF-документ
  */
 app.post('/api/v1/generate-pdf-with-header-footer', async (req: Request, res: Response) => {
-    const { html, margin, headerTemplate, footerTemplate } = req.body;
+    const {html, margin, headerTemplate, footerTemplate} = req.body;
     try {
         const pdfBase64 = await generatePdf(html, margin, headerTemplate, footerTemplate);
-        res.json({ pdfBase64 });
+        res.json({pdfBase64});
     } catch (error) {
-        res.status(500).json({ error: 'Failed to generate PDF' });
+        res.status(500).json({error: 'Не удалось сгенерировать PDF'});
     }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Сервер запущен по адресу http://localhost:${port}/swagger`);
 });
+
